358109f3fe6c9c68d11dff41380fcf41d203a976
Martin Galvan <martin.galvan@kinandcarta.com>
2022-11-04 17:20:36 -0500

    Added .gitignore

--- backend/src/main/java/com/example/qrcode_auth/config/SecurityConfig.java
+++ backend/src/main/java/com/example/qrcode_auth/config/SecurityConfig.java
@@ -2,14 +2,11 @@ package com.example.qrcode_auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
@@ -43,11 +40,6 @@ public class SecurityConfig {
                 .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
                 .and()
                 .authenticationProvider(authProvider)
-                .authorizeHttpRequests((request)->request
-                        .antMatchers(HttpMethod.GET,"/api","**/login","**/singup","**/","**/qr_sample").permitAll()
-                        .antMatchers(HttpMethod.POST,"**/login","**/singup").permitAll()
-                        .antMatchers(HttpMethod.GET,"**/logout","**/qr_data").authenticated()
-                        .anyRequest().authenticated())
                 .build();
     }
 }
